4e10e5b7b803753cc59f4c5884ef265a39bc70cb
buci19 <2106476452@qq.com>
2024-09-17 23:24:19 +0800

    Initial commit

--- src/views/oldman/RegisterView.vue
+++ src/views/oldman/RegisterView.vue
@@ -7,30 +7,21 @@
             <form action="">
                 <!-- 用户名输入框 -->
                 <div class="userName">
-                    <input type="text" placeholder="请输入用户名" style="height: 45px; width: 370px; border-radius: 15px; border-color: white; padding-left: 10px;
+                    <input type="text" v-model="Register.username" placeholder="请输入用户名" style="height: 45px; width: 370px; border-radius: 15px; border-color: white; padding-left: 10px;
                         text-indent: 20px;">
                 </div>
                 <!-- 密码输入框 -->
                 <div class="passWd">
-                    <input type="text" placeholder="请输入密码" style="height: 45px; width: 370px; border-radius: 15px; border-color: white; padding-left: 10px;
+                    <input type="text" v-model="Register.password" placeholder="请输入密码" style="height: 45px; width: 370px; border-radius: 15px; border-color: white; padding-left: 10px;
                         text-indent: 20px;">
                 </div>
-                <!-- 手机号输入框 -->
-                <div class="phone">
-                    <input type="text" placeholder="请输入手机号" style="height: 45px; width: 270px; border-radius: 15px; border-color: white; padding-left: 10px;
-                        text-indent: 20px;" class="phoneText">
-                    <span class="authCode"><input type="button" value="获取验证码" style="height: 38px; width: 100px; border-radius: 15px;
-                     border-color: white; padding-left: 10px; font-size: 17px; font-family: 宋体;
-                     background-color: rgb(153,153,255);  box-shadow: 2px 2px 5px 2px gray;"></span>
-                </div>
-                <!-- 输入验证码 -->
-                <div class="authCodeGet">
-                    <input type="text" placeholder="请输入验证码" style="height: 45px; width: 370px; border-radius: 15px; border-color: white; padding-left: 10px;
+                <div class="confirmPasswd">
+                    <input type="text" v-model="Register.confirmpasswd" placeholder="再次确认密码" style="height: 45px; width: 370px; border-radius: 15px; border-color: white; padding-left: 10px;
                         text-indent: 20px;">
                 </div>
                 <!-- 注册按钮 -->
                 <div class="register_bottom">
-                    <input type="button" value="注册" style="height: 38px; width: 150px; border-radius: 15px;
+                    <input type="button" value="注册" @click="register" style="height: 38px; width: 150px; border-radius: 15px;
                      border-color: white; padding-left: 10px; font-size: 23px; font-family: 宋体;
                      background-color: rgb(153,153,255);  box-shadow: 2px 2px 5px 2px gray;">
                 </div>
@@ -44,38 +35,44 @@
 </template>
 
 <script>
+/* eslint-disable */
 import axios from 'axios';
-
 export default {
     data() {
         return {
-            userName: '',
-            passWd: '',
-            phone: '',
-            authCode: ''
+            Register: {
+                username: '',
+                password: '',
+                confirmpasswd: ''
+            }
         }
     },
     methods: {
-        async register() {
-            try {
-                // 后端的API是/oldman/register
-                // 下面的那个注释是确保代码如果没有使用到也不会报错
-                // eslint-disable-next-line no-unused-vars 
-                const response = await axios.post('http://localhost:5080/oldman/register', {
-                    username: this.userName,
-                    password: this.passWd,
-                    phone: this.phone,
-                    authCode: this.authCode
-                })
-                //// 处理注册成功的响应  
-                alert('注册成功！');
-            } catch (error) {
-                // 处理注册失败的情况  
-                console.error('注册失败:', error.response.data);
+        register() {
+            if (this.Register.password !== this.Register.confirmpasswd) {
+                alert('两次密码不一致')
+            } else {
+                this.$axios
+                    .post('http://localhost:5081/api/oldman/register', {
+                        username: this.Register.username,
+                        password: this.Register.password
+                    })
+                    .then(successResponse => {
+                        if(successResponse.data.code === 200) {
+                            alert('注册成功')
+                            this.$router.replace({ path: '/' })
+                        }else{
+                            alert('注册失败')
+                        }
+                    })
             }
         }
     }
+
+
+
 }
+
 
 </script>
 
@@ -163,28 +160,19 @@ body {
     padding-left: 10px;
 }
 
-.phone {
+.confirmPasswd {
     position: relative;
     left: 50px;
     top: 100px;
 }
-
-/* 单独对第一个input框单独设置背景图片 */
-.phone .phoneText {
-    background-image: url("@/assets/images/phone.png");
+.confirmPasswd input{
+    background-image: url("@/assets/images/passwd.png");
     background-position: left;
     background-size: 25px 25px;
     background-repeat: no-repeat;
     padding-left: 10px;
 }
 
-.authCode {
-    height: 60px;
-    width: 190px;
-    position: relative;
-    left: 20px;
-    top: 5px;
-}
 
 .register_bottom {
     height: 60px;
@@ -199,17 +187,5 @@ body {
     transform: scale(1.1);
 }
 
-.authCodeGet {
-    position: relative;
-    left: 50px;
-    top: 130px;
-}
 
-.authCodeGet input {
-    background-image: url("@/assets/images/authCode.png");
-    background-position: left;
-    background-size: 25px 25px;
-    background-repeat: no-repeat;
-    padding-left: 10px;
-}
 </style>
